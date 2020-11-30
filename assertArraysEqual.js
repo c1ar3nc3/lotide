8609fd90f871cccc116ee500f650e5e40b72aec4
Clarence Williams <xwilliams.clarencex@gmail.com>
2020-11-30 21:20:17 +0000

    w2d1

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,11 @@
+const eqArrays = require("./eqArrays");
+
 const assertArraysEqual = function(arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    for (let x = 0; x < arr1.length; x++) {
-      if (arr1[x] !== arr2[x]) {
-        console.log("🛑🛑🛑 Arrays are not equal.");
-        return false;
-      }
-    }
+  if (eqArrays(arr1, arr2)){
+    console.log(`✅✅✅ Assertion passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${arr1} !== ${arr2}`);
   }
-  console.log("✅✅✅ Arrays are equal.");
-  return true;
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
+module.exports = assertArraysEqual;
