694cacc05c7309bfe6579ec6e391e25e0c610e71
Clarence Williams <xwilliams.clarencex@gmail.com>
2020-11-30 21:11:41 +0000

    w2d1

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("✅✅✅Assertion Passed: " + actual + " === " + expected);
-  } else {
-    console.assert(actual === expected,"🛑🛑🛑Assertion Failed: " + actual + " !== " + expected);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   for(let x = 0; x < arr1.length; x++) {
     if(arr1[x] !== arr2[x])
@@ -14,5 +6,4 @@ const eqArrays = function(arr1, arr2) {
   return true;
 }
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
