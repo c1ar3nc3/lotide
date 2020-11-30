44de8c774d02b15e05b9fb4fcc7215d11f0368c2
Clarence Williams <xwilliams.clarencex@gmail.com>
2020-11-30 21:53:35 +0000

    w2d1

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,14 @@
-const assert = require('../assertEqual');
-const tail = require('../tail')
+const assert = require('chai').assert;
+const tail = require('../tail');
 
+describe("#tail", () => {
 
-assert(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.deepEqual(tail([2, 5]), [5]); 
+  });
+
+});
